69996747ba6dc911f2afc67c5b3a426701e32245
Hamdi Damar <hamdi.damar@hotmail.com>
2020-12-26 11:55:56 +0300

    Bileşenler oluşturuldu ve yönlendirmeler yapıldı

--- kodluyoruz-hackathon-web/src/app/app.module.ts
+++ kodluyoruz-hackathon-web/src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { SssComponent } from './sss/sss.component';
+import { OldHacksComponent } from './old-hacks/old-hacks.component';
+import { NewHacksComponent } from './new-hacks/new-hacks.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { OldHacksDetailComponent } from './old-hacks-detail/old-hacks-detail.component';
+import { NewHacksDetailComponent } from './new-hacks-detail/new-hacks-detail.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    SssComponent,
+    OldHacksComponent,
+    NewHacksComponent,
+    MainPageComponent,
+    OldHacksDetailComponent,
+    NewHacksDetailComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
